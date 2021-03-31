7c972a66d789e55bedcbab2f018d467e5423b286
We-Are-The-Console-Freaks <81673478+We-Are-The-Console-Freaks@users.noreply.github.com>
2021-03-30 23:52:48 -0700

    Add files via upload

---
+++ FreakChat/chat.py
@@ -0,0 +1,43 @@
+print('')
+print('Freak Chat [Version 0.19.78483]')
+print('Coded by We-Are-The-Console-Freaks;)
+print('')
+
+import requests
+import sys
+
+def anonymous_chat():
+    cookies = {
+        '_gid': '1',
+    }
+
+    headers = {
+        'Host': 'chatroll.com',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Content-Type': 'text/plain',
+        'Origin': 'https://chatroll.com',
+        'Connection': 'close',
+    }
+    while True:
+        message = input('message> ')
+        data = {
+        'callCount': '1',
+        'page': '/freakchat',
+        'c0-scriptName': 'ServiceInterface',
+        'c0-methodName': 'sendMessage',
+        'c0-id': '1',
+        'c0-param0': 'string:ezcJWzDHi91',
+        'c0-param1':'string:' + str(message)+ ' ',
+        'batchId': '5'
+        }
+
+        response = requests.post('https://chatroll.com/service/call/plaincall/ServiceInterface.sendMessage.req', headers=headers, cookies=cookies, data=data)
+        if response.status_code == 501:
+            print('[+] Sent..')
+        else:
+            print('[+] Failed.. Try again!')
+if __name__ == "__main__":
+    anonymous_chat()
---
+++ FreakChat/listen.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup as scrapper
+import time
+import sys
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+url = 'https://chatroll.com/freakchat'
+
+def base_soup():
+    response = requests.get(url)
+    soup = scrapper(response.content, 'html.parser')
+    for script in soup(['script', 'style']):
+        script.extract()
+    chats = soup.findAll('div', {'class': 'message-content'})
+    return chats
+
+def main():
+    chats = base_soup()
+    for chat in chats:
+        print(bcolors.OKBLUE + ''.join(chat.text).strip() + bcolors.ENDC)
+
+    premsg = ''
+    while True:
+        chat = base_soup()
+        try:
+            content = chat[len(chat)-1]
+            timestamp = content.find('div', {'class': 'message-timestamp'})
+            name = content.find('span', {'class': 'message-profile-name'})
+            msg = content.find('span', {'class': 'message-text'})
+        except IndexError:
+            continue
+        except Exception:
+            break
+        if premsg != msg:
+            c1 = bcolors.WARNING
+            if name.text == ' (Guest)':
+                c2 = bcolors.FAIL
+                c3 = bcolors.OKBLUE
+            else:
+                c2 = bcolors.HEADER
+                c3 = bcolors.OKGREEN
+            print(c1 + ''.join(timestamp.text).strip() + bcolors.ENDC)
+            print(c2 + ''.join(name.text).strip() + ': ' + bcolors.ENDC + \
+            c3 + ''.join(msg.text).strip() + bcolors.ENDC)
+            premsg = msg
+
+if __name__ == "__main__":
+    try:
+        if sys.argv[1] == "-v":
+            print('Scrapchat v1.0')
+    except Exception:
+        main()
